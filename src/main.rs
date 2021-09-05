5beb61727ee03bd93a9548f6971fad699c978a01
karrybit <takumi.k.5610@gmail.com>
2021-09-05 16:02:45 +0900

    mv main.rs

--- src/main.rs
+++ src/main.rs
@@ -1,17 +1,1 @@
-use mini_redis::{client, Result};
-
-#[tokio::main]
-pub async fn main() -> Result<()> {
-    // open connection
-    let mut client = client::connect("127.0.0.1:6379").await?;
-
-    // set "world" to "hello"
-    client.set("hello", "world".into()).await?;
-
-    // get "hello"
-    let result = client.get("hello").await?;
-
-    println!("got value from the server; result={:?}", result);
-
-    Ok(())
-}
+fn main() {}
